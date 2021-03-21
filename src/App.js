48833913be439111a7f028ed8905a958c2fee83d
Achal Pathak <pathak.achal5@gmail.com>
2021-03-21 12:18:00 +0530

    restructured the code

--- src/App.js
+++ src/App.js
@@ -2,48 +2,59 @@ import { useState } from 'react';
 import './App.css';
 import { Dice } from "./components"
 
-
+import dice_image from './assets/dice_image.svg';
 
 export default function App() {
   const [dice_counter, setDice_counter] = useState(1);
   const [side_counter, setSide_counter] = useState(6);
   const [rolling_dice, setRolling_dice] = useState(false);
+  const [total_dice_count, setTotal_dice_count] = useState(0);
+  const [all_dices, setAll_dices] = useState([]);
 
   const increaseDiceCount = () => {
     setDice_counter(dice_counter + 1)
-    setRolling_dice(false);
   };
 
   const decreaseDiceCount = () => {
     if (dice_counter > 1) {
       setDice_counter(dice_counter - 1);
-      setRolling_dice(false);
     }
-
   };
 
   const increaseSideCount = () => {
     setSide_counter(side_counter + 1)
-    setRolling_dice(false);
   };
 
   const decreaseSideCount = () => {
     if (side_counter > 2) {
       setSide_counter(side_counter - 1);
-      setRolling_dice(false);
     }
   };
 
 
   const rollDice = () => {
     setRolling_dice(true);
+    setTotal_dice_count(0);
+    setAll_dices([]);
+    getDices();
   };
 
-
+  const getDices = () => {
+    let new_total = 0;
+    for (var i = 1; i <= dice_counter; i++) {
+      let random = Math.floor(Math.random() * side_counter) + 1;
+      new_total = new_total + random;
+      setAll_dices(all_dices => [...all_dices, <Dice count={random} />]);
+    }
+    setTotal_dice_count(new_total);
+  }
 
   return (
     <div className="App">
-      <h1>Dice Roll Game</h1>
+      <div className="header">
+        <img src={dice_image} alt="" height="60" />
+        <h1>Dice Roll Game</h1>
+      </div>
       <div className="dice_inputs">
         <label htmlFor="dice_counter">Number of Dice</label>
         <input id="dice_counter" type="text" value={dice_counter} readOnly />
@@ -57,7 +68,10 @@ export default function App() {
         <button className="down_button" onClick={decreaseSideCount}>Down</button>
       </div>
       <button className="roll_dice_btn" onClick={rollDice}>Roll Dice !</button>
-      {rolling_dice ? <Dice dice_counter={dice_counter} side_counter={side_counter} /> : null}
+      <div className="diceContainer">
+        {rolling_dice ? all_dices : null}
+      </div>
+      {total_dice_count ? <span className="total_count">Total = {total_dice_count}</span> : null}
     </div>
   );
 }
--- src/components/dice/index.js
+++ src/components/dice/index.js
@@ -1,23 +1,9 @@
 import React from 'react'
 import "./style.css";
-function index({ dice_counter, side_counter }) {
-
-  const getRandom = () => {
-    return Math.floor(Math.random() * side_counter) + 1  
-  }
-
-  const getDices = () => {
-    let all_dices = [];
-    for (var i = 1; i <= dice_counter; i++) {
-      all_dices.push(<div className="diceBox"><span>{getRandom(side_counter)}</span></div>);
-    }
-    return all_dices;
-  }
+function index({ count }) {
 
   return (
-    <div className="diceContainer">
-      {getDices()}
-    </div>
+    <div className="dice_box"><span>{count}</span></div>
   )
 }
 
