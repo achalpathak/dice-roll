f7c48d773067e52aa0c64e82dc1db78fa1ac97aa
Achal Pathak <pathak.achal5@gmail.com>
2021-03-21 14:22:19 +0530

    code refactored

--- src/App.js
+++ src/App.js
@@ -7,46 +7,34 @@ import dice_image from './assets/dice_image.svg';
 export default function App() {
   const [dice_counter, setDice_counter] = useState(1);
   const [side_counter, setSide_counter] = useState(6);
-  const [rolling_dice, setRolling_dice] = useState(false);
-  const [total_dice_count, setTotal_dice_count] = useState(0);
   const [all_dices, setAll_dices] = useState([]);
 
-  const increaseDiceCount = () => {
-    setDice_counter(dice_counter + 1)
-  };
-
-  const decreaseDiceCount = () => {
-    if (dice_counter > 1) {
+  const updateDiceCount = (action) => {
+    if (action === 'up') {
+      setDice_counter(dice_counter + 1)
+    }
+    else if (dice_counter > 1) {
       setDice_counter(dice_counter - 1);
     }
   };
 
-  const increaseSideCount = () => {
-    setSide_counter(side_counter + 1)
-  };
-
-  const decreaseSideCount = () => {
-    if (side_counter > 2) {
+  const updateSideCount = (action) => {
+    if (action === 'up') {
+      setSide_counter(side_counter + 1);
+    }
+    else if (side_counter > 2) {
       setSide_counter(side_counter - 1);
     }
   };
-
-
-  const rollDice = () => {
-    setRolling_dice(true);
-    setTotal_dice_count(0);
-    setAll_dices([]);
-    getDices();
-  };
-
+  
   const getDices = () => {
-    let new_total = 0;
-    for (var i = 1; i <= dice_counter; i++) {
+    setAll_dices([]);
+    let dices = [];
+    for (let i = 1; i <= dice_counter; i++) {
       let random = Math.floor(Math.random() * side_counter) + 1;
-      new_total = new_total + random;
-      setAll_dices(all_dices => [...all_dices, <Dice count={random} />]);
+      dices.push(random);
     }
-    setTotal_dice_count(new_total);
+    setAll_dices(dices);
   }
 
   return (
@@ -58,20 +46,20 @@ export default function App() {
       <div className="dice_inputs">
         <label htmlFor="dice_counter">Number of Dice</label>
         <input id="dice_counter" type="text" value={dice_counter} readOnly />
-        <button className="up_button" onClick={increaseDiceCount}>Up</button>
-        <button className="down_button" onClick={decreaseDiceCount}>Down</button>
+        <button className="up_button" onClick={() => updateDiceCount('up')}>Up</button>
+        <button className="down_button" onClick={() => updateDiceCount('down')}>Down</button>
       </div>
       <div className="dice_inputs">
         <label htmlFor="side_counter">Number of Side</label>
         <input id="side_counter" type="text" value={side_counter} readOnly />
-        <button className="up_button" onClick={increaseSideCount}>Up</button>
-        <button className="down_button" onClick={decreaseSideCount}>Down</button>
+        <button className="up_button" onClick={() => updateSideCount('up')}>Up</button>
+        <button className="down_button" onClick={() => updateSideCount('down')}>Down</button>
       </div>
-      <button className="roll_dice_btn" onClick={rollDice}>Roll Dice !</button>
-      <div className="diceContainer">
-        {rolling_dice ? all_dices : null}
+      <button className="roll_dice_btn" onClick={getDices}>Roll Dice !</button>
+      <div className="dice_container">
+        {all_dices.length > 0 && all_dices.map((dice, i) => (<Dice count={dice} key={i} />))}
       </div>
-      {total_dice_count ? <span className="total_count">Total = {total_dice_count}</span> : null}
+      {all_dices.length > 0 && <span className="total_count">Total = {all_dices.reduce((x, y) => x + y)}</span>}
     </div>
   );
 }
--- src/components/dice/index.js
+++ src/components/dice/index.js
@@ -1,7 +1,6 @@
-import React from 'react'
 import "./style.css";
-function index({ count }) {
 
+function index({ count }) {
   return (
     <div className="dice_box"><span>{count}</span></div>
   )
